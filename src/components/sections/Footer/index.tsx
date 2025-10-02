7a6d5d7efc299313f16182757bde18fb0dd42627
Mark de Leon <mark_deleon@hotmail.com>
2025-10-02 02:22:14 -0400

    Updated app version number 1.0.5

--- src/components/sections/Footer/index.tsx
+++ src/components/sections/Footer/index.tsx
@@ -99,7 +99,7 @@ export default function Footer(props) {
                     )}
                 </div>
                 <div id="app-version-number">
-                <code>v1.0.0</code>
+                <code>v1.0.5</code>
             </div>
             </div>
         </footer>
